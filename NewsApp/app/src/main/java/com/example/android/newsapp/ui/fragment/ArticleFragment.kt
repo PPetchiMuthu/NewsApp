c847987cd69713e997ec765ef243703ab04dfc64
PPetchiMuthu <petchimuthu0091@gmail.com>
2022-11-17 01:27:04 +0530

    commit

--- NewsApp/app/src/main/java/com/example/android/newsapp/ui/fragment/ArticleFragment.kt
+++ NewsApp/app/src/main/java/com/example/android/newsapp/ui/fragment/ArticleFragment.kt
@@ -1,18 +1,27 @@
-package com.example.android.newsapp.ui.fragment
+package com.example.android.news.ui.fragment
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
-import com.example.android.newsapp.ui.MainActivity
+import androidx.navigation.fragment.navArgs
 import com.example.android.newsapp.R
+import com.example.android.newsapp.ui.MainActivity
 import com.example.android.newsapp.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
     lateinit var viewModel: NewsViewModel
+    val args:ArticleFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as MainActivity).viewModel
+        val article = args.article
+        webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
     }
 }
